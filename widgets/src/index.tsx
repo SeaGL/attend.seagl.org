10005ab21d38fd164f81e8a6cddefecfa157ccad
Andrew Kvalheim <Andrew@Kvalhe.im>
2021-11-04 16:10:28 -0700

    Open links from widgets in new tabs

--- widgets/src/index.tsx
+++ widgets/src/index.tsx
@@ -1,10 +1,20 @@
+import { MDXProvider } from "@mdx-js/react";
+import { HTMLAttributes } from "react";
 import ReactDOM from "react-dom";
 import { QueryParamProvider } from "use-query-params";
 import Widget from "./components/Widget";
 
+const mdxComponents = {
+  a: (props: HTMLAttributes<HTMLAnchorElement>) => (
+    <a rel="noopener noreferrer" target="_blank" {...props} />
+  ),
+};
+
 ReactDOM.render(
   <QueryParamProvider>
-    <Widget />
+    <MDXProvider components={mdxComponents}>
+      <Widget />
+    </MDXProvider>
   </QueryParamProvider>,
   document.getElementById("widget")
 );
