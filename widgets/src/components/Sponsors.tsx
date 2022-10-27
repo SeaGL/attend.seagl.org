dd1ca843ced602588524ff6384994af84c5c11f6
Andrew Kvalheim <Andrew@Kvalhe.im>
2022-10-27 11:23:08 -0700

    Hide empty sponsor headings

--- widgets/src/components/Sponsors.tsx
+++ widgets/src/components/Sponsors.tsx
@@ -5,15 +5,19 @@ interface Props {
 }
 
 const Sponsors = ({ sponsors }: Props) => {
-  const renderLevel = (level: string) => (
-    <div className={`logo-wall level-${level}`}>
-      {sponsors.filter((s) => s.level === level).map((s) =>
-        <a className={`logo-wall-item ${s.logo.horizontal ? "horizontal" : ""}`} title={s.name} href={s.url} rel="noopener noreferrer" target="_blank">
-          <img alt={s.name} src={s.logo.horizontal || s.logo.square} />
-        </a>
-      )}
-    </div>
-  );
+  const renderLevel = (level: string, title: string) => {
+    const atLevel = sponsors.filter((s) => s.level === level);
+    return atLevel.length > 0 && <>
+      <h2>{title}</h2>
+      <div className={`logo-wall level-${level}`}>
+        {atLevel.map((s) => (
+          <a className={`logo-wall-item ${s.logo.horizontal ? "horizontal" : ""}`} title={s.name} href={s.url} rel="noopener noreferrer" target="_blank">
+            <img alt={s.name} src={s.logo.horizontal || s.logo.square} />
+          </a>
+        ))}
+      </div>
+    </>;
+  };
 
   return (
     <>
@@ -23,29 +27,12 @@ const Sponsors = ({ sponsors }: Props) => {
 
       <p>Their contributions will have a real and lasting effect in the FLOSS community. Thank you all.</p>
 
-      <h2>Platinum</h2>
-
-      {renderLevel("platinum")}
-
-      <h2>Gold</h2>
-
-      {renderLevel("gold")}
-
-      <h2>Silver</h2>
-
-      {renderLevel("silver")}
-
-      <h2>Bronze</h2>
-
-      {renderLevel("bronze")}
-
-      <h2>Media Sponsors</h2>
-
-      {renderLevel("media")}
-
-      <h2>Special Thanks To</h2>
-
-      {renderLevel("thanks")}
+      {renderLevel("platinum", "Platinum")}
+      {renderLevel("gold", "Gold")}
+      {renderLevel("silver", "Silver")}
+      {renderLevel("bronze", "Bronze")}
+      {renderLevel("media", "Media Sponsors")}
+      {renderLevel("thanks", "Special Thanks To")}
     </>
   );
 };
