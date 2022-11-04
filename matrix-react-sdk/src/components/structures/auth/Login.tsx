1cbd836b529ab8f85b08cfc3ec4d58ed2b8b634a
Andrew Kvalheim <Andrew@Kvalhe.im>
2022-11-03 22:50:34 -0700

    Disable automatic homeserver prompt

--- matrix-react-sdk/src/components/structures/auth/Login.tsx
+++ matrix-react-sdk/src/components/structures/auth/Login.tsx
@@ -612,7 +612,6 @@ export default class LoginComponent extends React.PureComponent<IProps, IState> 
                     <ServerPicker
                         serverConfig={this.props.serverConfig}
                         onServerConfigChange={this.props.onServerConfigChange}
-                        prompt={true}
                     />
                     { this.renderLoginComponentForFlows() }
                     { footer }
